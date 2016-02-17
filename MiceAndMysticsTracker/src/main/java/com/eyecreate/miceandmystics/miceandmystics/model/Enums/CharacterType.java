35e7d14e3b0f3777ded9dc27d5c673919e1d57e4
Kevin Whitaker <eyecreate@gmail.com>
2016-02-16 22:44:40 -0500

    Just in case, char types can be localized too.

--- MiceAndMysticsTracker/src/main/java/com/eyecreate/miceandmystics/miceandmystics/model/Enums/CharacterType.java
+++ MiceAndMysticsTracker/src/main/java/com/eyecreate/miceandmystics/miceandmystics/model/Enums/CharacterType.java
@@ -1,12 +1,25 @@
 package com.eyecreate.miceandmystics.miceandmystics.model.Enums;
 
+import com.eyecreate.miceandmystics.miceandmystics.MiceAndMysticsApplication;
+import com.eyecreate.miceandmystics.miceandmystics.R;
+
 public enum CharacterType {
-    Warrior,
-    Leader,
-    Mystic,
-    Scamp,
-    Tinkerer,
-    Archer,
-    Healer,
-    Maginos
+    Warrior(R.string.char_type_warrior),
+    Leader(R.string.char_type_leader),
+    Mystic(R.string.char_type_mystic),
+    Scamp(R.string.char_type_scamp),
+    Tinkerer(R.string.char_type_tinkerer),
+    Archer(R.string.char_type_archer),
+    Healer(R.string.char_type_healer),
+    Maginos(R.string.char_type_maginos);
+
+    private int displayName;
+
+    CharacterType(int displayName) {
+        this.displayName = displayName;
+    }
+
+    public String displayName() { return MiceAndMysticsApplication.getInstance().getString(displayName); }
+
+    @Override public String toString() { return displayName(); }
 }
